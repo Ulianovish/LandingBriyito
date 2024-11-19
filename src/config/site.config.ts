a17d13cf83706d20f9e56489fdcecc530dfd5cdb
Miguel Caro <ulianovish@gmail.com>
2024-11-19 05:10:02 -0500

    change photo and styles

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -3,18 +3,18 @@ import type { SiteConfig } from "./config";
 export const siteConfig: SiteConfig = {
     // Información básica del sitio
     name: 'Mamá Que Factura',
-    description: 'Aprende El Increíble Método Mamá Que Factura',
+    description: 'Aprende El Método Mamá Que Factura',
     url: 'https://mamaquefactura.com/',
     author: 'Briyito Barrero',
     authorSubtitle: 'Mamá Emprendedora',
 
     // Meta tags y SEO
     seo: {
-        title: 'El Increíble Método Mamá Que Factura',
-        description: 'Aprende El Increíble Método Mamá Que Factura',
+        title: 'El Método Mamá Que Factura',
+        description: 'Aprende El Método Mamá Que Factura',
         openGraph: {
             title: 'Mama Que Factura',
-            description: 'Aprende El Increíble Método Mamá Que Factura',
+            description: 'Aprende El Método Mamá Que Factura',
             image: '/og-image.jpg'
         },
         facebookVerification: '6ka7t2u3jg6z23trqaveqgr7inykz6'
@@ -31,7 +31,7 @@ export const siteConfig: SiteConfig = {
         {
             number: 1,
             title: 'CLASE #1',
-            description: 'Aprenderás el mismo método que están usando cientos de mamás para crear un negocio solido en internet con tiendas en línea',
+            description: 'Aprenderás el mismo método que yo utilizo y cientos de mamás para crear un negocio solido en internet con tiendas en línea',
             icon: 'learn'
         },
         {
@@ -43,13 +43,13 @@ export const siteConfig: SiteConfig = {
         {
             number: 3,
             title: 'CLASE #3',
-            description: 'La forma más fácil de comprar TÚ PROPIA TIENDA en internet en menos de 15 días',
+            description: 'La forma más fácil de comprar TÚ PROPIA TIENDA en internet en menos de 5 días',
             icon: 'rocket'
         },
         {
             number: 4,
             title: 'CLASE #4',
-            description: 'Como convertirte en una mamá que factura exitosa creando TIENDAS EN LÍNEA, y recibir una renta',
+            description: 'Como convertirte en una MAMÁ PRESENTE que factura desde casa y como vivir 100 % de tu negocio online',
             icon: 'trophy'
         }
     ],
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
     // Textos de secciones
     sections: {
         hero: {
-            title: 'El Increíble Método, <br /> Mamá que Factura.',
+            title: 'El Método, <br /> Mamá que Factura.',
             description: `Así fue como logré entrar en los negocios digitales, creando tiendas por internet que me
                 permitieron ser una mamá presente en la vida de mis hijas.<br /><br /> En este entrenamiento
                 gratuito, aprenderás cómo comprar una tienda virtual exitosa y vivir de ello, sin necesidad
@@ -65,31 +65,18 @@ export const siteConfig: SiteConfig = {
         },
         about: {
             title: '¿Quién Soy y por qué puedo apoyarte?',
-            content: ` ¡Hola! Soy Briyito Barrero, mamá y emprendedora apasionada. Aunque estudié ingeniería civil
-                y gerencia de proyectos, mi dedicación como mamá y ama de casa me llevó a buscar nuevas
-                formas de generar ingresos desde el hogar a través de negocios en línea. Tras muchas
-                experiencias, descubrí las claves para una vida abundante y ahora quiero compartir contigo
-                estrategias y herramientas para que tú también logres resultados en el mundo virtual. Por
-                eso, creé "Mamá que Factura", un método diseñado para mamás como tú que desean generar
-                ingresos desde casa y ser mamás presentes en la vida de sus niños.<br /><br />
-
-                Puedo ser tu guía en este viaje porque comprendo las exigencias de ser una mamá ama de casa,
-                profesional y emprendedora. He transitado por ese camino y entiendo los desafíos que
-                conlleva. Mi experiencia me ha enseñado valiosas lecciones que compartiré contigo,
-                ayudándote a sortear obstáculos y alcanzar tus metas más rápidamente. Con soluciones
-                prácticas, te acompañaré paso a paso para que puedas generar ingresos desde la comodidad de
-                tu hogar. Estoy aquí para brindarte el apoyo necesario y ayudarte a construir una vida plena
-                y exitosa como mamá emprendedora.`
+            content: ` Soy Briyito Barrero, profesional en ingeniería civil, gerente de proyectos, enfocados en finanzas, emprendedora y mi trabajo mas importante mama de dos hijas, mi dedicación como mama me llevo a buscar nuevas formas de generar ingresos desde casa para poder ser una mama mas presente y tener tiempo con mis hijas, después de varias desafíos y experiencias descubrí las claves para una vida exitosa, y ahora quiero compartir contigo la estrategia y las herramientas para que tú también logres resultados en el mundo virtual, por eso cree el método mama que factura especialmente para mamas que desean generar ingresos desde casa y ser mamas presentes en la vida de los niños. 
+                Puedo ayudarte porque comprendo las exigencias de ser una profesional y mama al mismo tiempo compartiré contigo mi experiencia y te ayudare a alcanzar tus metas más rápidamente con soluciones prácticas te acompañare paso a paso para que comprendas el mundo digital y puedas romper barreras que te están limitando a que puedas generar ingresos desde la comodidad del hogar, estoy aquí para brindarte el apoyo necesario para construir una vida plena y exitosa como mama y emprendedora.`
         },
         intro: {
-            description: 'En este entrenamiento gratuito aprenderás cómo puedes comprar una tienda virtual exitosa y vivir de ello. No necesitas experiencia previa.'
+            description: 'En este entrenamiento gratuito aprenderás de negocios digitales y cómo puedes comprar una tienda virtual exitosa y vivir de ello. No necesitas experiencia previa.'
         }
     },
 
     // Configuración de fechas
     dates: {
         title: 'La Masterclass comienza en',
-        masterclass: '2024-10-26T21:00:00'
+        masterclass: '2024-11-25T21:00:00'
     },
 
     // Textos de botones y formularios
